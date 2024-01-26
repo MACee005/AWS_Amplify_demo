3508acf8a9cd97d5d29c7768eacf6035e2a43396
MACee005 <salesonparamount@gmail.com>
2024-01-26 16:17:50 +0530

    Added common components, Auth and Routing

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,10 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import SiteNav from './components/Common/SiteNav';
+import SiteFooter from './components/Common/SiteFooter';
+import HomePage from './components/home/HomePage';
+
+import LoginPage from './components/auth/LoginPage';
+import RegisterPage from './components/auth/RegisterPage';
 
 function App() {
   return (
     <div>
-      <header>
-      </header>
+      <SiteNav />
+      <Routes>
+        <Route>
+        <Route path='*' element={<HomePage />} />
+        <Route path='/' exact={true} element={<HomePage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='register' element={<RegisterPage />} />
+        </Route>
+      </Routes>
+      <SiteFooter />
     </div>
   );
 }
